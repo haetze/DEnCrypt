a2af1ccc8d2dcd803e5120d1e100007bc94b9d8c
Richard Stewing <richy.sting@gmail.com>
2016-02-28 19:10:00 -0800

    CryptoReadWrite updated

---
+++ src/Modules/CryptoReadWrite.hs
@@ -0,0 +1,38 @@
+{-# LANGUAGE OverloadedStrings #-}
+--Richard Stewing
+--25.02.2016
+--CryptoReadWrite.hs
+--Wrapper for crypto read write functions
+
+
+module CryptoReadWrite where
+
+import Crypto
+import System.Directory
+import qualified Data.ByteString.Char8 as B
+import System.Posix.Terminal
+import System.Posix.IO (stdInput)
+
+
+--writeToDisk:: location -> content -> password -> IO ()
+writeToDisk:: String -> String -> String -> IO ()
+writeToDisk location s  p = do 
+  home <- getHomeDirectory
+  B.writeFile (home ++ "/" ++ location ) (ciph p s)
+
+--readFromDisk:: location -> password -> IO String
+readFromDisk:: String -> String -> IO String
+readFromDisk location p = do
+  home <- getHomeDirectory
+  s <- B.readFile (home ++ location)
+  return . B.unpack . decipher p $ B.unpack s
+
+
+
+getPassword:: IO String
+getPassword = do
+  tc <- getTerminalAttributes stdInput
+  setTerminalAttributes stdInput (withoutMode tc EnableEcho) Immediately
+  password <- getLine
+  setTerminalAttributes stdInput tc Immediately
+  return password 
