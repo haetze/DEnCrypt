9965386d273f08b1c0903626c4dcba57c853f913
Richard Stewing <richy.sting@gmail.com>
2016-02-28 19:13:17 -0800

    main.hs created

---
+++ src/Main/main.hs
@@ -0,0 +1,29 @@
+--Richard Stewing
+-- <28.02.2016>
+
+
+--module main where
+
+
+
+import CryptoReadWrite
+import System.Environment (getArgs)
+import System.IO
+
+main = do
+  args <- getArgs
+  password <- getPassword
+  case (words args) of
+    ("decrypt":inName:outName:_) -> do
+      s <- readFromDisk inName password
+      writeFile outName s
+    ("encrypt":inName:outName:_) -> do
+      s <- readFile inName
+      writeToDisk outName s password 
+    _ -> do
+      putStrLn "Command syntax"
+      putStrLn "encrypt/decrypt inName outName"
+      
+
+
+    
