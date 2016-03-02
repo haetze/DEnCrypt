80102e7ab0c5a0c79b882b5a085cbff17f6b7033
Richard Stewing <richy.sting@gmail.com>
2016-03-01 19:44:05 -0800

    set dir

--- src/Main/main.hs
+++ src/Main/main.hs
@@ -17,7 +17,13 @@ main = do
       writeFile outName s
     ("encrypt":inName:outName:_) -> do
       s <- readFile inName
-      writeToDisk outName s password 
+      writeToDisk outName s password
+    ("-f":"decrypt":inName:outName:_) -> do
+      s <- readFromDisk ("../../" ++ inName) password
+      writeFile ("../../" ++ outName) s
+    ("-f":"encrypt":inName:outName:_) -> do
+      s <- readFile ("../../" ++ inName)
+      writeToDisk ("../../" ++ outName) s password
     _ -> do
       putStrLn "Command syntax"
       putStrLn "encrypt/decrypt inName outName"
